f52e0b4883665b93d78ac59c182a7cb58a4ee546
syedanasquadri <syedanasquadri123@gmail.com>
2025-10-05 17:41:14 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react"
+
 const App = () => {
-  return 
+    const [inputVal, setinputVal] = useState("");
+    const [cityName, setcityName] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setcityName(inputVal);
+        setinputVal("");
+    }
+    return <div>
+    <form onSubmit={handleSubmit}>
+    <input
+     type="text"
+     onChange={(e) => setinputVal(e.target.value)}
+     value={inputVal}
+      />
+      <button type="Submit">submit</button>
+      </form>
+      <ul>
+        {cityName}
+        </ul>
+    </div>
 }
+
+export default App
